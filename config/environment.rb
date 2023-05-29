216f64deb2738182d6838cc7dd69f3b64038d4ba
nicole <nicolenamayan133@gmail.com>
2023-05-29 13:04:31 +0300

    done

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,11 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/development.sqlite3"
+) 
+
 require_relative "../app/models/artist"
